17944f2927733ed419d020eb6c8618151d92b0b8
Sascha Willems <webmaster@saschawillems.de>
2020-06-03 07:03:39 +0200

    Deployment tests

--- redist/deploy.sh
+++ redist/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -F `data=@Vulkan_Caps_Viewer-latest-x86_64.AppImage` ${upload_url}
+curl -F 'data=@Vulkan_Caps_Viewer-latest-x86_64.AppImage' ${upload_url}?release_type=latest
 
 # if [ "$TRAVIS_BRANCH" = "qt_auto_deploy" ]; then
 #     curl -T Vulkan_Caps_Viewer*.AppImage -u ${ftp_user}:${ftp_pass} ftp.${ftp_host}:21
