be2fbe2362661d81f2103a3e6de01cae96698ccd
Sascha Willems <webmaster@saschawillems.de>
2020-06-04 20:49:26 +0200

    Deployment tests

--- redist/deploy.sh
+++ redist/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$TRAVIS_TAG" = ""]; then 
-    curl -F 'data=@Vulkan_Caps_Viewer-${TARGET_PLATFORM}-x86_64.AppImage' ${upload_url}?release_type=tag
+    curl -F "data=@Vulkan_Caps_Viewer-$TARGET_PLATFORM-x86_64.AppImage" ${upload_url}?release_type=tag
 else
-    curl -F 'data=@Vulkan_Caps_Viewer-${TARGET_PLATFORM}-x86_64.AppImage' ${upload_url}?release_type=latest
+    curl -F "data=@Vulkan_Caps_Viewer-$TARGET_PLATFORM-x86_64.AppImage" ${upload_url}?release_type=latest
 fi 
