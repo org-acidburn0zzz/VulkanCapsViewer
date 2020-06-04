dd4b3c400bba4f8d5224465884e8671e721c9d60
Sascha Willems <webmaster@saschawillems.de>
2020-06-04 20:40:49 +0200

    Deployment tests

--- redist/deploy.sh
+++ redist/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-curl -F 'data=@Vulkan_Caps_Viewer-latest-x86_64.AppImage' ${upload_url}?release_type=latest
-
-# if [ "$TRAVIS_BRANCH" = "qt_auto_deploy" ]; then
-#     curl -T Vulkan_Caps_Viewer*.AppImage -u ${ftp_user}:${ftp_pass} ftp.${ftp_host}:21
-#     exit 0
-# fi
- 
+if [ "$TRAVIS_TAG" = ""]; then 
+    curl -F 'data=@Vulkan_Caps_Viewer-${TARGET_PLATFORM}-x86_64.AppImage' ${upload_url}?release_type=tag
+else
+    curl -F 'data=@Vulkan_Caps_Viewer-${TARGET_PLATFORM}-x86_64.AppImage' ${upload_url}?release_type=latest
+fi 
